05edb846423f95873811052f3f12f4486c9abf1a
Alexander Scheurer <mail@aspepex.net>
2017-01-21 17:28:18 +0100

    movement seems to be cool

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -5,13 +5,21 @@ public class CharController : MonoBehaviour
     private Rigidbody2D _rig;
 
     private Vector2 _forceVector;
+    private Vector2 _velocityVector;
     private bool _grounded;
+    private float _reJumpTime;
+    private float _reJumpTimer;
+
 
     // Use this for initialization
     void Start()
     {
         _rig = GetComponent<Rigidbody2D>();
         _forceVector = new Vector2();
+        _velocityVector = new Vector2();
+        _reJumpTime = 1f;
+        _reJumpTimer = -1;
+        Spawn();
     }
 
     // Update is called once per frame
@@ -30,17 +38,34 @@ public class CharController : MonoBehaviour
 
         _forceVector.x = Input.GetAxis("Horizontal") * Config.ForceHorizontal;
 
-        if (_grounded)
+        if (_grounded && _reJumpTimer < 0)
         {
             _forceVector.y = Input.GetAxis("Jump") * Config.ForceJump;
+            if (_forceVector.y > 0)
+                _reJumpTimer = _reJumpTime;
         }
         else
         {
             _forceVector.y = 0;
+            _reJumpTimer = _reJumpTimer - Time.deltaTime;
         }
 
-        Debug.Log(hit.distance + " Grounded: " + _grounded);
+        if (transform.position.y < -2)
+            Spawn();
 
-        _rig.AddForce(_forceVector);
+        Debug.Log(hit.distance + " Grounded: " + _grounded + " ReJump: " + _reJumpTimer + " Velo: " + _rig.velocity.ToString("G3"));
+
+        _rig.AddForce(_forceVector.x * Vector2.right, Config.ForceModeMove);
+        _rig.AddForce(_forceVector.y * Vector2.up, Config.ForceModeJump);
+
+        _velocityVector.x = Mathf.Clamp(_rig.velocity.x, -Config.MaxVelocity.x, Config.MaxVelocity.x);
+        _velocityVector.y = Mathf.Clamp(_rig.velocity.y, -Config.MaxVelocity.y, Config.MaxVelocity.y);
+        _rig.velocity = _velocityVector;
+    }
+
+    void Spawn()
+    {
+        this.transform.position = Config.SpawnPosition;
+        _rig.velocity = Vector2.zero;
     }
 }
--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -2,9 +2,14 @@
 
 public static class Config
 {
-    public static Vector3 SpawnPosition = new Vector3();
+    public static Vector2 SpawnPosition = new Vector2(-4, 17);
+    public static Vector2 MaxVelocity = new Vector2(12f, 12f);
+
+    public static ForceMode2D ForceModeMove = ForceMode2D.Force;
+    public static ForceMode2D ForceModeJump = ForceMode2D.Impulse;
+
     public static float ForceHorizontal = 15f;
-    public static float ForceJump = 700f;
+    public static float ForceJump = 400f;
     public static float PitchSpan = 20f;
     public static float VisibilitySpan = 5f;
     public static int NumberOfBands = 10;
